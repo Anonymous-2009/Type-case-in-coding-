97b9777abff8125673118ff55b035d65de706b14
Anonymous <krishnabag751@gmail.com>
2024-02-12 08:39:36 +0000

    ahh new map list render method has been use use to load tha data for better understanding

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import Navbar from "./components/navbar/navbar.jsx";
 import Footer from "./components/footer/footer.jsx";
 import Data from "./components/body/data.jsx";
 
+
 export default function App() {
-  const object = {
-    train:'Train case is a naming convention where the first letter of each word is capitalized and the rest is in lowercase. Each word is also',
-    camel:'capitalizing words without spaces for readability in programming.', 
-    snake:'separating words with underscores to improve readability in programming conventions ', 
-    pascal:'apitalizing words without spaces for improved programming clarity ', 
-    kebab:'Joining words with hyphens for clear naming in programming styles', 
-  }
   return (
     <>
       <Navbar home='home' about='about' more='more' contact=' contact'/> 
       <div className="content">
      <p className="type"> different type of casing in coding </p>
-   <Data title="camelcase" data={object.camel}/>
-   <Data title="traincase" data={object.train}/>
-   <Data title="pascalcase" data={object.pascal}/>
-   <Data title="snakecase" data={object.snake}/>
-    <Data title="kebabcase" data={object.kebab}/>   
+        {/* Data is fetch map method to load datav */}
+      <Data/>
       </div>
       <Footer data='all right reserved &copy; 2024 '/>
     </>
--- src/components/body/data.jsx
+++ src/components/body/data.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import './data.css'
 
-export default function Body(props){
+export default function Body(){
+  const [data, setData] = useState([{
+  title: 'camelcase',
+  description:'capitalizing words without spaces for readability in programming'
+},{
+  title: 'traincase',
+  description:'separating words with underscores to improve readability in programming conventions'
+},{
+  title: 'pascalcase',
+  description:'cpitalizing words without spaces for improved programming clarity'
+},{
+  title: 'snakecase',
+  description:'Joining words with hyphens for clear naming in programming styles'
+},{
+  title: 'kebabcase',
+  description:'Joining words with hyphens for clear naming in programming styles'
+}]
+);
   return (
     <>
-     <dt> {props.title} </dt>
-     <dd> {props.data} </dd>
+      {data.map((props) => {
+       return (
+         <> 
+        <div class='data'>
+         <p> {props.title} </p>
+         <p> {props.description}</p>
+        </div>
+         </>
+       )
+       })}
     </>
   );
 };
@@ -19,3 +46,7 @@ Body.defaultProps = {
   title : 'title add soon',
   data : 'data add soon ',
 }
+
+ 
+
+
